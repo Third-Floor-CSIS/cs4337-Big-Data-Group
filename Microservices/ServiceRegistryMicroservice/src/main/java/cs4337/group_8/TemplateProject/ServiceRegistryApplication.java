0194f626833607c18906bda0bc8c63c12a9dceb8
Killian Ca <killiancarty@gmail.com>
2024-10-26 19:07:42 +0100

    Made changes based on pull request reviews. Deleting redundant files and editing the change log

--- Microservices/ServiceRegistryMicroservice/src/main/java/cs4337/group_8/TemplateProject/ServiceRegistryApplication.java
+++ Microservices/ServiceRegistryMicroservice/src/main/java/cs4337/group_8/TemplateProject/ServiceRegistryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateProjectApplication {
+public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TemplateProjectApplication.class, args);
+		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
 
 }
