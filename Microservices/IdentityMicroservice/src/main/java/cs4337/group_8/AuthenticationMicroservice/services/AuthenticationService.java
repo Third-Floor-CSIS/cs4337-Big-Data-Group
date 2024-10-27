42b1fc14886881fdbcd8e1360753cbea64553547
Milan <kmilanm13@gmail.com>
2024-10-27 22:56:57 +0000

    [Milan] Added Unit tests

--- Microservices/IdentityMicroservice/src/main/java/cs4337/group_8/AuthenticationMicroservice/services/AuthenticationService.java
+++ Microservices/IdentityMicroservice/src/main/java/cs4337/group_8/AuthenticationMicroservice/services/AuthenticationService.java
@@ -31,29 +31,29 @@ import java.util.Date;
 @Service
 @Slf4j
 public class AuthenticationService {
-    private final String clientId;
-    private final String clientSecret;
     private final AuthenticationRepository authenticationRepository;
     private final TokenRepository tokenRepository;
     private final UserRepository userRepository;
+    private final GoogleAuthService googleAuthService;
     private static final long DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
     private static final long SECOND_IN_MILLISECONDS = 1000;
 
-    public AuthenticationService(@Value("${CLIENT_SECRET}") String clientSecret,
-                                 @Value("${CLIENT_ID}") String clientId,
-                                 AuthenticationRepository authenticationRepository, TokenRepository tokenRepository, UserRepository userRepository) {
-        this.clientSecret = clientSecret;
-        this.clientId = clientId;
+    public AuthenticationService(
+            AuthenticationRepository authenticationRepository,
+            TokenRepository tokenRepository,
+            UserRepository userRepository,
+            GoogleAuthService googleAuthService) {
         this.authenticationRepository = authenticationRepository;
         this.tokenRepository = tokenRepository;
         this.userRepository = userRepository;
+        this.googleAuthService = googleAuthService;
     }
 
     public UserDTO handleAuthentication(String grantCode) {
-        GoogleAuthorizationResponse apiResponse = getOauthInformationFromGrantCode(grantCode);
+        GoogleAuthorizationResponse apiResponse = googleAuthService.getOauthInformationFromGrantCode(grantCode);
         String accessToken = apiResponse.getAccess_token();
         System.out.println("Access token: " + accessToken);
-        GoogleUserDetails userDetails = getGoogleProfileDetails(accessToken);
+        GoogleUserDetails userDetails = googleAuthService.getGoogleProfileDetails(accessToken);
         UserEntity user = userRepository.findByEmail(userDetails.getEmail())
                 .orElseGet(() -> createNewUserFromGoogleOauth(userDetails));
 
@@ -63,6 +63,21 @@ public class AuthenticationService {
         userDto.setUserId(user.getUser_id());
         userDto.setProfilePicture(user.getProfile_picture());
         return userDto;
+    }
+
+    public String refreshToken(String accessToken) throws RefreshTokenExpiredException {
+        boolean refreshTokenIsInvalid = googleAuthService.isRefreshTokenExpiredForAccessToken(accessToken);
+        if (!refreshTokenIsInvalid) {
+            TokenEntity tokenEntity = tokenRepository.getTokenEntityByCurrentAccessToken(accessToken)
+                    .orElseThrow(() -> new ValidateTokenException("No token found for user, please login again"));
+
+            String refreshToken = tokenEntity.getRefreshToken();
+
+            accessToken = googleAuthService.refreshAccessToken(refreshToken, tokenEntity.getUserId());
+            return accessToken;
+        }
+
+        throw new RefreshTokenExpiredException("Refresh token has expired, please login again");
     }
 
     private UserEntity createNewUserFromGoogleOauth(GoogleUserDetails userDetails) {
@@ -87,136 +102,5 @@ public class AuthenticationService {
         tokenEntity.setExpirationTimeRefreshToken(new Timestamp(System.currentTimeMillis() + (DAY_IN_MILLISECONDS * 14)));
         tokenRepository.save(tokenEntity);
     }
-
-    public String refreshToken(String accessToken) throws RefreshTokenExpiredException {
-        boolean refreshTokenIsInvalid = isRefreshTokenExpiredForAccessToken(accessToken);
-        if (!refreshTokenIsInvalid) {
-            TokenEntity tokenEntity = tokenRepository.getTokenEntityByCurrentAccessToken(accessToken)
-                    .orElseThrow(() -> new ValidateTokenException("No token found for user, please login again"));
-
-            String refreshToken = tokenEntity.getRefreshToken();
-
-            accessToken = refreshAccessToken(refreshToken, tokenEntity.getUserId());
-            return accessToken;
-        }
-
-        throw new RefreshTokenExpiredException("Refresh token has expired, please login again");
-    }
-
-    private String refreshAccessToken(String refreshToken, int userId) throws AuthenticationException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED); // The content type will contain the form data in the URL
-
-        MultiValueMap<String, String> params = getPayloadForGoogleRefresh(refreshToken);
-        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(params, headers);
-
-        try {
-            ResponseEntity<String> responseFromGoogle = requestRefreshToken(requestEntity);
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(responseFromGoogle.getBody());
-            GoogleAuthorizationResponse tokenDetails = objectMapper.treeToValue(jsonNode, GoogleAuthorizationResponse.class);
-
-            TokenEntity tokenEntity = tokenRepository.getTokenEntityByUserId(userId)
-                    .orElseThrow(() -> new AuthenticationException("No token found for user, please login again"));
-            tokenEntity.setCurrentAccessToken(tokenDetails.getAccess_token());
-            tokenEntity.setExpirationTimeAccessToken(new Timestamp(System.currentTimeMillis() + (SECOND_IN_MILLISECONDS * tokenDetails.getExpires_in())));
-            tokenRepository.save(tokenEntity);
-
-            return tokenDetails.getAccess_token();
-        } catch (JsonProcessingException e) {
-            throw new AuthenticationException("Failed to refresh access token");
-        }
-    }
-
-    private MultiValueMap<String, String> getPayloadForGoogleRefresh(String refreshToken) {
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("client_id", this.clientId);
-        params.add("client_secret", this.clientSecret);
-        params.add("refresh_token", refreshToken);
-        params.add("grant_type", "refresh_token");
-        return params;
-    }
-
-    private ResponseEntity<String> requestRefreshToken(HttpEntity<MultiValueMap<String, String>> requestPayload) {
-        RestTemplate restTemplate = new RestTemplate();
-        String googleRefreshTokenURL = "https://oauth2.googleapis.com/token";
-        return restTemplate.postForEntity(googleRefreshTokenURL, requestPayload, String.class);
-    }
-
-    private boolean isRefreshTokenExpiredForAccessToken(String accessToken) {
-        try {
-            TokenEntity tokenEntity = tokenRepository.getTokenEntityByCurrentAccessToken(accessToken)
-                    .orElseThrow(() -> new ValidateTokenException("No token found, please login again"));
-
-            Date expirationTime = tokenEntity.getExpirationTimeRefreshToken();
-            return expirationTime.before(new Date());
-        } catch (JWTDecodeException e) {
-            log.error("Invalid Access token provided: " + e.getMessage());
-            return true;
-        }
-    }
-
-    private GoogleUserDetails getGoogleProfileDetails(String accessToken) {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setBearerAuth(accessToken);
-
-        HttpEntity<String> requestEntity = new HttpEntity<>(httpHeaders);
-
-        String url = "https://www.googleapis.com/oauth2/v2/userinfo";
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            JsonNode jsonNode = objectMapper.readTree(response.getBody());
-
-            GoogleUserDetails authenticatedUser = objectMapper.treeToValue(jsonNode, GoogleUserDetails.class);
-            return authenticatedUser;
-        } catch (JsonProcessingException e) {
-            throw new AuthenticationException("Failed to parse provided user details");
-        }
-    }
-
-    private GoogleAuthorizationResponse getOauthInformationFromGrantCode(String grantCode) {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        setInformationParameters(grantCode, params);
-
-
-        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(params, headers);
-
-        try {
-            String url = "https://oauth2.googleapis.com/token";
-            ResponseEntity<String> response = restTemplate.postForEntity(url, requestEntity, String.class);
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(response.getBody());
-            GoogleAuthorizationResponse userTokenDetails = objectMapper.treeToValue(jsonNode, GoogleAuthorizationResponse.class);
-            return userTokenDetails;
-        } catch (JsonProcessingException e) {
-            throw new AuthenticationException("Failed to get access token");
-        } catch (HttpClientErrorException e) {
-            throw new AuthenticationException("Invalid grant code");
-        }
-
-    }
-
-    private void setInformationParameters(String code, MultiValueMap<String, String> params) {
-        // User authorization code
-        params.add("code", code);
-        params.add("redirect_uri", "http://localhost:8082/grantcode");
-        params.add("client_id", this.clientId);
-        params.add("client_secret", this.clientSecret);
-
-        // Information we retrieve
-        params.add("scope", "https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile");
-        params.add("scope", "https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email");
-        params.add("scope", "openid");
-        params.add("grant_type", "authorization_code");
-    }
-
 
 }
--- Microservices/IdentityMicroservice/src/test/java/cs4337/group_8/AuthenticationMicroservice/controllers/TestAuthenticationController.java
+++ Microservices/IdentityMicroservice/src/test/java/cs4337/group_8/AuthenticationMicroservice/controllers/TestAuthenticationController.java
@@ -1,10 +1,131 @@
 package cs4337.group_8.AuthenticationMicroservice.controllers;
 
+import cs4337.group_8.AuthenticationMicroservice.DTOs.UserDTO;
+import cs4337.group_8.AuthenticationMicroservice.exceptions.AuthenticationException;
+import cs4337.group_8.AuthenticationMicroservice.exceptions.RefreshTokenExpiredException;
+import cs4337.group_8.AuthenticationMicroservice.services.AuthenticationService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AuthenticationController.class)
+@ContextConfiguration(classes = {TestAuthenticationController.SecurityTestConfig.class})
 public class TestAuthenticationController {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    // Disable security config for testing
+    @TestConfiguration
+    @EnableWebSecurity
+    static class SecurityTestConfig {
+        @Bean
+        @Primary
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
+                    request -> request.requestMatchers("*").permitAll()
+            );
+            return http.build();
+        }
+    }
+
     @Test
-    public void testRegister() {
+    public void grantCode_shouldReturnUserDTO_whenAuthenticationIsSuccessful() throws Exception {
+        // Incoming properties
+        String code = "some code";
+        String scope = "some scope";
+        String authUser = "some user";
+        String prompt = "some prompt";
+
+        UserDTO expectedUser = new UserDTO();
+        expectedUser.setUserId(1);
+        expectedUser.setProfilePicture("some link to a pfp");
+
+        when(authenticationService.handleAuthentication(code))
+                .thenReturn(expectedUser);
+
+        mockMvc.perform(get("/grantcode")
+                        .param("code", code)
+                        .param("scope", scope)
+                        .param("authuser", authUser)
+                        .param("prompt", prompt))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(expectedUser.getUserId()))
+                .andExpect(jsonPath("$.profilePicture").value(expectedUser.getProfilePicture()));
+    }
+
+    @Test
+    public void grantCode_shouldReturnBadRequest_whenAuthenticationFails() throws Exception {
+        // Incoming properties
+        String code = "invalid code";
+        String scope = "some scope";
+        String authUser = "some user";
+        String prompt = "some prompt";
+
+        String errorMsg = "Authentication failed";
+        when(authenticationService.handleAuthentication(code))
+                .thenThrow(new AuthenticationException(errorMsg));
+
+        mockMvc.perform(get("/grantcode")
+                        .param("code", code)
+                        .param("scope", scope)
+                        .param("authuser", authUser)
+                        .param("prompt", prompt))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value(errorMsg));
+    }
+
+    @Test
+    public void refreshToken_shouldReturnOk_whenTokenIsRefreshed() throws Exception {
+        // Init variables
+        String requestHeaderToken = "Bearer someToken";
+        String trimmedToken = requestHeaderToken.substring(7);
+        String refreshedToken = "refreshedToken";
+
+        when(authenticationService.refreshToken(trimmedToken))
+                .thenReturn(refreshedToken);
+
+        mockMvc.perform(post("/refresh-token")
+                        .header("Authorization", requestHeaderToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value("Token refreshed"))
+                .andExpect(header().string("Authorization", "Bearer " + refreshedToken));
 
     }
+
+    @Test
+    public void refreshToken_shouldReturn403Forbidden_whenRefreshTokenIsExpired() throws Exception {
+        // Init variables
+        String requestHeaderToken = "Bearer someToken";
+        String trimmedToken = requestHeaderToken.substring(7);
+
+        when(authenticationService.refreshToken(trimmedToken))
+                .thenThrow(new RefreshTokenExpiredException("Refresh token expired"));
+
+        mockMvc.perform(post("/refresh-token").header("Authorization", requestHeaderToken))
+                .andExpect(status().isForbidden())
+                .andExpect(header().string("Location", "/login"))
+                .andExpect(content().string("Refresh token expired"));
+    }
+
+
 }
