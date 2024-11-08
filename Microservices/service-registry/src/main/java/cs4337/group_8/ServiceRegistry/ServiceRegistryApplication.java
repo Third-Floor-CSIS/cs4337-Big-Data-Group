92d7198a7604f0d0820b969f1cc52364a210cbab
Milan <kmilanm13@gmail.com>
2024-11-08 23:17:06 +0000

    [Milan] Merged the two PRs, updated the cloud dependency

--- Microservices/service-registry/src/main/java/cs4337/group_8/ServiceRegistry/ServiceRegistryApplication.java
+++ Microservices/service-registry/src/main/java/cs4337/group_8/ServiceRegistry/ServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package cs4337.group_8.ServiceRegistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
