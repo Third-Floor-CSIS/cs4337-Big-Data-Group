fb6ff3558a899c0868d620f3b87e709511cf87e5
Brendan Golden <git@brendan.ie>
2024-11-09 18:35:30 +0000

    feat: add a query to modify a profile

--- Microservices/Profile/src/main/java/cs4337/group_8/ProfileService/repositories/ProfileRepository.java
+++ Microservices/Profile/src/main/java/cs4337/group_8/ProfileService/repositories/ProfileRepository.java
@@ -16,4 +16,24 @@ public interface ProfileRepository extends JpaRepository<ProfileEntity, String> 
     // Optional is a container object which may or may not contain a non-null value
     @Query(value = "SELECT e FROM ProfileEntity e WHERE e.user_id = ?1")
     Optional<ProfileEntity> findByIdEquals(String user_id);
-    }
+
+    @Modifying
+    @Query(
+        """
+        UPDATE ProfileEntity t 
+        SET t.full_name = :full_name,  t.bio = :bio,  t.profile_pic = :profile_pic  
+        WHERE t.user_id = :user_id
+        """
+    )
+    int updateByUserId(
+        @Param("user_id")
+        int user_id,
+        @Param("full_name")
+        String full_name,
+        @Param("bio")
+        String bio,
+        @Param("profile_pic")
+        String profile_pic
+    );
+
+}
