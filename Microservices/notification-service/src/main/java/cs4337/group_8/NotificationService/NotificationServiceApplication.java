be50cfb7d4f60e22a5cbbd62a268e86a7addcef8
Fawad Shahzad <60967843+faad555@users.noreply.github.com>
2024-11-14 14:31:17 +0000

    Notification microservice (#59)
    * intial setup for notificaiton microservice
    
    * service registry integration
    
    * added notification model
    
    * service layer for notificcation ms
    
    * fixed the port
    
    it will use the notificaiton port if it's set other wise it will use the default.
    
    ---------
    
    Co-authored-by: Fawad <aash4037@gmail.com>

---
+++ Microservices/notification-service/src/main/java/cs4337/group_8/NotificationService/NotificationServiceApplication.java
@@ -0,0 +1,14 @@
+package cs4337.group_8.NotificationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+}
---
+++ Microservices/notification-service/src/main/java/cs4337/group_8/NotificationService/services/NotificationService.java
@@ -0,0 +1,12 @@
+package cs4337.group_8.NotificationService.services;
+
+import cs4337.group_8.NotificationService.models.NotificationDTO;
+
+public interface NotificationService {
+
+    // Add notification handling methods here
+    // Examples:
+    // - Process notifications from
+    // - Send notifications to users
+    // etc.
+}
