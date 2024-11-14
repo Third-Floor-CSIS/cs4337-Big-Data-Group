f30a658128a162646be6ed48e0ac2db85db6a345
silpac-12 <seancaplis@gmail.com>
2024-11-14 16:03:12 +0000

    Removed log files

--- Microservices/Posts/src/main/java/cs4337/group_8/TemplateProject/controllers/PostController.java
+++ Microservices/Posts/src/main/java/cs4337/group_8/TemplateProject/controllers/PostController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+//Post Controller
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
