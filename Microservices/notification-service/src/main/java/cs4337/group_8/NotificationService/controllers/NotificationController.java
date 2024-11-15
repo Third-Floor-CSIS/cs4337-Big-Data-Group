205fde923e7c9f5a123d821f3b419341ff93d4bb
bourkupine <euanbourke@gmail.com>
2024-11-15 15:52:13 +0000

    notification: bug fix
    Signed-off-by: bourkupine <euanbourke@gmail.com>

--- Microservices/notification-service/src/main/java/cs4337/group_8/NotificationService/controllers/NotificationController.java
+++ Microservices/notification-service/src/main/java/cs4337/group_8/NotificationService/controllers/NotificationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("notification")
+@RequestMapping("/notification")
 public class NotificationController {
 
     @Autowired
