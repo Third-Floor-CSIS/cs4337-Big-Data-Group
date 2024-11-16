2b9772fa2957e88b0109d337af29760de57f27ce
Euan Bourke <euanbourke@gmail.com>
2024-11-16 14:39:19 +0000

    notification: implemented read notification functionality
    Signed-off-by: Euan Bourke <euanbourke@gmail.com>

--- Microservices/notification-service/src/main/java/cs4337/group_8/NotificationService/controllers/NotificationController.java
+++ Microservices/notification-service/src/main/java/cs4337/group_8/NotificationService/controllers/NotificationController.java
@@ -3,10 +3,8 @@ package cs4337.group_8.NotificationService.controllers;
 import cs4337.group_8.NotificationService.DTO.NotificationDTO;
 import cs4337.group_8.NotificationService.services.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,11 @@ public class NotificationController {
     @GetMapping("/unread/{receiverId}")
     public List<NotificationDTO> getUnreadNotifications(@PathVariable String receiverId) {
         return notificationService.getUnreadNotifications(receiverId);
+    }
+
+    @PostMapping("/read/{id}")
+    public ResponseEntity<Void> markNotificationAsRead(@PathVariable String id) {
+        notificationService.markNotificationAsRead(id);
+        return ResponseEntity.ok().build(); // returning as ResponseEntity incase we want status codes
     }
 }
--- Microservices/notification-service/src/main/java/cs4337/group_8/NotificationService/services/NotificationService.java
+++ Microservices/notification-service/src/main/java/cs4337/group_8/NotificationService/services/NotificationService.java
@@ -1,6 +1,7 @@
 package cs4337.group_8.NotificationService.services;
 
 import cs4337.group_8.NotificationService.DTO.NotificationDTO;
+import cs4337.group_8.NotificationService.entities.NotificationEntity;
 import cs4337.group_8.NotificationService.mappers.NotificationMapper;
 import cs4337.group_8.NotificationService.repositories.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,13 @@ public class NotificationService {
 
 
     public List<NotificationDTO> getUnreadNotifications(String receiverId) {
+        List<NotificationEntity> notifications = notificationRepository.findUnreadNotificationsByReceiverId(receiverId);
+        return notifications.stream()
+                .map(notificationMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    public void markNotificationAsRead(String notificationId) {
+        notificationRepository.markNotificationAsRead(notificationId);
     }
 }
