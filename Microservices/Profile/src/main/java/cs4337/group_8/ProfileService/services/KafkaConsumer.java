04f3db9d4b769cd71295ff75bca27d62fa565c44
Killian Ca <killiancarty@gmail.com>
2024-11-25 22:12:02 +0000

    began adding kafka to the identity and profile microservices based on the week 11 kafka lab

---
+++ Microservices/Profile/src/main/java/cs4337/group_8/ProfileService/services/KafkaConsumer.java
@@ -0,0 +1,24 @@
+package cs4337.group_8.ProfileService.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+    private int messagesProcessedCount = 0;
+
+    @KafkaListener(topics = "profile-updates", groupId = "profile-group")
+    public void consume(String message) {
+        LOGGER.info("Consumed message: {}", message);
+        messagesProcessedCount++;
+    }
+
+    public int getMessagesProcessedCount() {
+        return messagesProcessedCount;
+    }
+}
+
