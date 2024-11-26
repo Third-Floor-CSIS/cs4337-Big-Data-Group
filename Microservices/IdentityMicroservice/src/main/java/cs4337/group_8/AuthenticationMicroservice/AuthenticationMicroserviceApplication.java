837ebb690f1718baf297470af6811fc5474d58ea
Milan <kmilanm13@gmail.com>
2024-11-26 14:32:53 +0000

    [Milan] Thanks Bayan for pointing the typo out

--- Microservices/IdentityMicroservice/src/main/java/cs4337/group_8/AuthenticationMicroservice/AuthenticationMicroserviceApplication.java
+++ Microservices/IdentityMicroservice/src/main/java/cs4337/group_8/AuthenticationMicroservice/AuthenticationMicroserviceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class AuthenticationMicrosericeApplication {
+class AuthenticationMicroserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationMicrosericeApplication.class, args);
+		SpringApplication.run(AuthenticationMicroserviceApplication.class, args);
 	}
 
 }
