6a4c4859f5f06c8ba1dadaef474e085be0b7e7db
silpac-12 <seancaplis@gmail.com>
2024-12-02 16:39:18 +0000

    added IF NOT EXITS to tables

--- Microservices/Posts/src/main/resources/db/db.changelog-master.sql
+++ Microservices/Posts/src/main/resources/db/db.changelog-master.sql
@@ -1,24 +1,26 @@
 --liquibase formatted sql
 
-        -- changeset Sean:0
-        CREATE TABLE posts (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        likesCount INT,
-        content VARCHAR(1024),
-        userID VARCHAR(255) NOT NULL
-        );
+-- changeset Sean:0
+CREATE TABLE IF NOT EXISTS posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    likesCount INT,
+    content VARCHAR(1024),
+    userID VARCHAR(255) NOT NULL
+);
 
-        -- changeset Sean:1
-        CREATE TABLE likes (
-        likeId BIGINT AUTO_INCREMENT PRIMARY KEY,
-        userId BIGINT NOT NULL,
-        postId BIGINT NOT NULL,
-        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-        isLiked BOOLEAN DEFAULT TRUE NOT NULL
-        );
+-- changeset Sean:1
+CREATE TABLE IF NOT EXISTS likes (
+    likeId BIGINT AUTO_INCREMENT PRIMARY KEY,
+    userId BIGINT NOT NULL,
+    postId BIGINT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    isLiked BOOLEAN DEFAULT TRUE NOT NULL
+);
 
-        ALTER TABLE likes
-        ADD CONSTRAINT FK_LIKES_USERID FOREIGN KEY (userId) REFERENCES users (id);
+-- changeset Sean:2
+ALTER TABLE likes
+ADD CONSTRAINT FK_LIKES_USERID FOREIGN KEY (userId) REFERENCES users (id);
 
-        ALTER TABLE likes
-        ADD CONSTRAINT FK_LIKES_POSTID FOREIGN KEY (postId) REFERENCES posts (id);
+-- changeset Sean:3
+ALTER TABLE likes
+ADD CONSTRAINT FK_LIKES_POSTID FOREIGN KEY (postId) REFERENCES posts (id);
